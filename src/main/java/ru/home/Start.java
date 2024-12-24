7d3c434a13c26e8c18de357644d1a9eb66528586
Mariya Misharina <maryshorohova@mail.ru>
2024-12-24 15:46:17 +0600

    пункт 3 задание 1

--- src/main/java/ru/home/Start.java
+++ src/main/java/ru/home/Start.java
@@ -6,15 +6,31 @@ public class Start {
         application1.printTreeWorld();
         Start start1 = new Start();
         start1.chekSumSing();
+        Start start2 = new Start();
+                start2.printColor();
     }
 
-    public void chekSumSing(){
+    public void chekSumSing() {
         int int1 = 1;
         int int2 = -2;
         int int3 = int1 + int2;
         if (int3 >= 0) {
             System.out.println("Сумма положительная");
 
-        } else { System.out.println("Сумма отрицательная");}
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public void printColor() {
+        int value = -1;
+        if (value <=0){
+            System.out.println("Красный");
+        } else if (value >0 && value <= 100) {
+            System.out.println("Желтый");
+        } else if (value >100) {
+            System.out.println("Зеленый");
+        }
+
     }
 }
