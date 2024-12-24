294dac6dd36f964291932f0f4c5e9c7b7ebf67ea
Mariya Misharina <maryshorohova@mail.ru>
2024-12-24 13:39:39 +0600

    пункт 1 задание 1

---
+++ src/main/java/ru/home/Application.java
@@ -0,0 +1,10 @@
+package ru.home;
+
+public class Application {
+
+   public void printTreeWorld () {
+       System.out.println("Orange");
+       System.out.println("Banana");
+       System.out.println("Apple");
+    }
+}
---
+++ src/main/java/ru/home/Start.java
@@ -0,0 +1,8 @@
+package ru.home;
+
+public class Start {
+    public static void main(String[] args) {
+Application application1 = new Application();
+application1.printTreeWorld();
+    }
+}
