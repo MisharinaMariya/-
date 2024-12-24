3b6b890417f8d87a67b8aa6b1039f125b4724e88
Mariya Misharina <maryshorohova@mail.ru>
2024-12-24 14:10:37 +0600

    пункт 2 задание 1

--- src/main/java/ru/home/Start.java
+++ src/main/java/ru/home/Start.java
@@ -2,7 +2,19 @@ package ru.home;
 
 public class Start {
     public static void main(String[] args) {
-Application application1 = new Application();
-application1.printTreeWorld();
+        Application application1 = new Application();
+        application1.printTreeWorld();
+        Start start1 = new Start();
+        start1.chekSumSing();
+    }
+
+    public void chekSumSing(){
+        int int1 = 1;
+        int int2 = -2;
+        int int3 = int1 + int2;
+        if (int3 >= 0) {
+            System.out.println("Сумма положительная");
+
+        } else { System.out.println("Сумма отрицательная");}
     }
 }
