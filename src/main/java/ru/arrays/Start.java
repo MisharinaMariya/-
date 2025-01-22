c8fedc496e0b37a3e8ffaa39100e7640ffc94954
Mariya Misharina <maryshorohova@mail.ru>
2025-01-22 15:06:44 +0600

    Задание Л8.1

---
+++ src/main/java/ru/arrays/Start.java
@@ -0,0 +1,26 @@
+package ru.arrays;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Start {
+    public static void main(String[] args) {
+        String[] arr = {"Привет","Привет","Пока","Хэллоу","Добрый день","Здравствуйте", "До свидания","До встречи","Добрый день","Пока"};
+        Set<String> set = new HashSet<>();
+        HashMap<String,Integer> hashMap = new HashMap<>();
+        for (int i= 0; i < arr.length; i++){
+            set.add(arr[i]);
+            Integer value = hashMap.get(arr[i]);
+            if (value == null){
+                hashMap.put(arr[i], 1);
+            } else {
+                hashMap.put(arr[i], value+1);
+            }
+
+        }
+        System.out.println(set);
+        System.out.println(hashMap);
+    }
+
+}
