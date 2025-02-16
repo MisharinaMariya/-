7cae09f3f4d400d41af9456af95a7fb8ac4c5d32
Mariya Misharina <maryshorohova@mail.ru>
2025-02-16 12:52:10 +0600

    Задание Л13

--- src/test/java/ru/home/GoTest.java
+++ src/test/java/ru/home/GoTest.java
@@ -1,12 +1,21 @@
 package ru.home;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class GoTest {
+    private Go go;
+
+    @BeforeClass
+    private void setUp() {
+        go = new Go();
+    }
 
     @Test
     public void testGetFactorial() {
+        int result = go.getFactorial(6);
+        assertEquals(result, 720);
     }
 }
