d426184ea3505c081c68e70b5ac32ec34e12be25
Mariya Misharina <maryshorohova@mail.ru>
2025-02-26 13:35:18 +0600

    Задание Л16

--- src/test/java/PayFrame.java
+++ src/test/java/PayFrame.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 import java.util.List;
 
+import static java.time.Duration.ofSeconds;
+
 public class PayFrame {
 
     public WebDriver driver;
@@ -23,90 +25,42 @@ public class PayFrame {
         this.driver = driver;
     }
 
-    /**
-     * Определение локатора для пополняемой суммы в описаниии оплаты
-     */
     @FindBy(css = "div.pay-description__cost span:nth-child(1)")
     private WebElement payFrameDescriptionCost;
 
-    /**
-     * Определение локатора для кнопки оплаты
-     */
     @FindBy(css = "div.card-page__card button")
     private WebElement payFrameBtn;
 
-    /**
-     * Определение локатора для номера телефона в описании оплаты
-     */
     @FindBy(css = "div.pay-description__text span:nth-child(1)")
     private WebElement payFrameDescriptionPhone;
 
-    /**
-     * Определение локаторов для картинок платежных систем в форме ввода номера карты
-     */
     @FindBy(css = "div.cards-brands img")
     private List<WebElement> payImgages;
 
-    /**
-     * Определение локатора для плейсхолдера поля ввода номера карты
-     */
     @FindBy(css = "input[formcontrolname=creditCard] + label")
     private WebElement creditCardPlaceholder;
 
-    /**
-     * Определение локатора для плейсхолдера поля ввода даты истечения карты
-     */
     @FindBy(css = "input[formcontrolname=expirationDate] + label")
     private WebElement expirationDatePlaceholder;
 
-    /**
-     * Определение локатора для плейсхолдера поля ввода CVC
-     */
     @FindBy(css = "input[formcontrolname=cvc] + label")
     private WebElement cvcPlaceholder;
 
-    /**
-     * Определение локатора плейсхолдера для поля ввода имени держателя карты
-     */
     @FindBy(css = "input[formcontrolname=holder] + label")
     private WebElement cardHolderPlaceholder;
 
-    /**
-     * Возвращает текст пополняемой суммы в описаниии оплаты
-     *
-     * @return текст пополняемой суммы
-     * @throws NoSuchElementException объект не найден
-     */
     public String getPayFrameDescriptionCost() throws NoSuchElementException {
         return payFrameDescriptionCost.getText();
     }
 
-    /**
-     * Возвращает текст кнопки оплаты
-     *
-     * @return текст кнопки оплаты
-     * @throws NoSuchElementException объект не найден
-     */
     public String getPayFrameBtnText() throws NoSuchElementException {
         return payFrameBtn.getText();
     }
 
-    /**
-     * Возвращает текст номера телефона в описании оплаты
-     *
-     * @return текст номера телефона в описании оплаты
-     * @throws NoSuchElementException объект не найден
-     */
     public String getPayFrameDescriptionPhone() throws NoSuchElementException {
         return payFrameDescriptionPhone.getText();
     }
 
-    /**
-     * Возвращает отображается ли картинка с именем src
-     * @param scr имя картинки
-     * @return отображатся ли картинк
-     * @throws NoSuchElementException объект не найден
-     */
     public boolean isDisplayedImg(String scr) throws NoSuchElementException {
         for (WebElement payImage : payImgages) {
             if (payImage.getAttribute("src").contains(scr)) {
@@ -115,42 +69,19 @@ public class PayFrame {
         }
         return false;
     }
-    /**
-     * Возвращает текст плейсхолдера поля ввода номера карты
-     *
-     * @return текст плейсхолдера поля ввода номера карты
-     * @throws NoSuchElementException объект не найден
-     */
+
     public String getCreditCardPlaceholder() throws NoSuchElementException {
         return creditCardPlaceholder.getText();
     }
 
-    /**
-     * Возвращает текст плейсхолдера поля ввода даты истечения карты
-     *
-     * @return текст плейсхолдера поля ввода даты истечения карты
-     * @throws NoSuchElementException объект не найден
-     */
     public String getExpirationDatePlaceholder() throws NoSuchElementException {
         return expirationDatePlaceholder.getText();
     }
 
-    /**
-     * Возвращает текст плейсхолдера поля ввода CVC
-     *
-     * @return текст плейсхолдера поля ввода CVC
-     * @throws NoSuchElementException объект не найден
-     */
     public String getCvcPlaceholder() throws NoSuchElementException {
         return cvcPlaceholder.getText();
     }
 
-    /**
-     * Возвращает текст плейсхолдера поля ввода имени держателя карты
-     *
-     * @return текст плейсхолдера поля ввода имени держателя карты
-     * @throws NoSuchElementException объект не найден
-     */
     public String getCardHolderPlaceholder() throws NoSuchElementException {
         return cardHolderPlaceholder.getText();
     }
